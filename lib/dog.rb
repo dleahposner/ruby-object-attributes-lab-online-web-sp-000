0aa07e03f655e6c15f8d98b34c655c033b7b2ad4
Devorah Posner <dleahposner@gmail.com>
2021-02-22 18:51:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog 
   
   def initialize(dog_name)
-    @name = name
+    @dog_name = name
   end
  
   def name
-    @name
+    @dog_name
   end
  
   def name=(new_name)
-    @name = new_name
+    @dog_name = new_name
   end
   
   
